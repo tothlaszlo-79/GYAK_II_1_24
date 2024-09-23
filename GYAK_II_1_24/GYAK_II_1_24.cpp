790417badefa2ca85762a2b1c140ada24d22a952
Tóth László <lastoth@gmail.com>
2024-09-23 15:31:21 +0200

    Add project files.

---
+++ GYAK_II_1_24/GYAK_II_1_24.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a_old, b_old;
+    cout << "Adja meg a teglalap ket oldalat" << endl;
+    cin >> a_old >> b_old;
+    cout << "A teglalap terulete: " << a_old * b_old << endl;
+    system("pause");
+    return 0;
+
+}
